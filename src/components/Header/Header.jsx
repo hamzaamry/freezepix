953a5179606e14c6b378bdc027ec006a6510970f
hamzaamry <hamzaamry009@gmail.com>
2023-12-04 23:52:12 +0100

    styling header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import White from '../../Assets/logo/White.png'
+
+import { Box, IconButton } from "@mui/material";
+
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      p={2}
+      style={{
+        backgroundColor: '#000',
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.2)', // Add a simple shadow
+      }}
+    >
+      <img src={White} alt="Logo" style={{ maxWidth: '100%', maxHeight: '50px', marginLeft: '2rem' }} />
+      {/* ICONS */}
+      <Box display="flex" style={{ marginRight: '2rem' }}>
+        <IconButton>
+          <NotificationsIcon style={{ color: 'white', fontSize: '30px' }} />
+        </IconButton>
+        <IconButton>
+          <AccountCircleIcon style={{ color: 'white', fontSize: '30px' }} />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
-}
 
-export default Admin
+
+  return (
+    <div>
+      <h2>Admin Panel</h2>
+
+      <div>
+        <h3>User Management</h3>
+        <ul>
+          zina
+        </ul>
+      </div>
+
+      {/* Add more sections and functionality as needed */}
+    </div>
+  );
+};
+
+export default Admin;
