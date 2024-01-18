e74adb8a9970f673f8104cc95c092c3c75cd103e
hamzaamry <hamzaamry009@gmail.com>
2024-01-18 20:12:21 +0100

    countries management

---
+++ src/pages/Management/Countries.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FlagIcon from '@mui/icons-material/Flag';
+
+const Countries = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Countries
