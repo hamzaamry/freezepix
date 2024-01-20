504b359afa0f63fc66c9b34369b2eeb3e3a50a79
hamzaamry <hamzaamry009@gmail.com>
2024-01-20 13:43:01 +0100

    facture design

--- src/pages/Management/OrderDetails.jsx
+++ src/pages/Management/OrderDetails.jsx
@@ -5,10 +5,75 @@ import PersonIcon from "@mui/icons-material/Person";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import PlaceIcon from "@mui/icons-material/Place";
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import { Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableRow,  Dialog, DialogContent, DialogActions } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
+import  Black  from "../../Assets/logo/Black.png"
+
+
+
+const Facture = ({ open, handleClose }) => {
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="md">
+      <div style={{ 
+        padding: '2rem', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center' ,
+        border: "2px solid #000",
+        width: '25rem',
+        height:'50rem'
+        }}>
+        
+        <div style={{ marginBottom: '16px' }}>
+          <img
+            src={Black}
+            alt="Logo"
+            style={{ width: '150px', height: 'auto' }}
+          />
+        </div>
+
+        <Typography
+                  fontFamily="DM sans"
+            fontSize="20px"
+            fontWeight="700"
+            lineHeight="50px"
+            marginBottom="1.5rem"
+          >
+            Facture
+          </Typography>
+
+        <DialogContent>
+      
+            <p>Date: 05 December 2023</p>
+            <p> Nom: Amri Hamza </p>
+            <p>Telephone: +216 92 221 937 </p>
+            <hr />
+            <p> Address1: karatchi street, Bardo, Tunis </p>
+            <p> Address2: Manouba </p>
+            <p> Code Postale: 5000 </p>
+            <hr />
+            <p>Prix unitaire: 26.00 DT</p>
+            <p>Quantité: x3</p>
+            <p>Frais de livraison: 7.00 DT</p>
+            <p>Taxes: 1.36 DT</p>
+             <h4>Totale : <span color="green"> 30.00 DT </span></h4>
+        </DialogContent>
+
+     
+        <DialogActions>
+          <Button onClick={handleClose} variant="contained" color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </div>
+    </Dialog>
+  );
+};
+
+
 
 
 const OrderDetails = () => {
@@ -21,6 +86,15 @@ const OrderDetails = () => {
   }, [token, navigate]); 
 
   const [status, setStatus] = useState("");
+  const [factureOpen, setFactureOpen] = useState(false);
+
+  const handleFactureClose = () => {
+    setFactureOpen(false);
+  };
+
+  const handleFactureOpen = () => {
+    setFactureOpen(true);
+  }
 
   const handleStatusChange = (event) => {
     setStatus(event.target.value);
@@ -96,7 +170,7 @@ const OrderDetails = () => {
             <option value="Shipped">Shipped</option>
             <option value="Confirmed">Confirmed</option>
           </select>
-          <IconButton style={{ color: "white" }}>
+          <IconButton style={{ color: "white" }} onClick={handleFactureOpen} >
             <PrintIcon />
           </IconButton>
         </div>
@@ -283,6 +357,8 @@ const OrderDetails = () => {
         </div>
       </div>
     </div>
+    <Facture open={factureOpen} handleClose={handleFactureClose} />
+
       </div>
 
   );
